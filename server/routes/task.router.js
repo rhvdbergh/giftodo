0d35ad0c42840bad946807dc20d4a531797771f4
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-12-20 15:17:59 -0600

    uses nodemon as dev dependency

--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -3,9 +3,28 @@ const router = express.Router();
 const pool = require('../modules/pool');
 
 // GET /api/task
+// gets all the tasks on the server
 router.get('/', (req, res) => {
   console.log('/in GET /api/task');
-  res.send({ success: true });
+
+  // build a SQL query
+  const query = `
+  SELECT * FROM "task" ORDER BY "id";
+  `;
+
+  // run the query
+  pool
+    .query(query)
+    .then((response) => {
+      res.send(response.rows); // send back the rows, which contain the data
+    })
+    .catch((err) => {
+      console.log(
+        'There was an error retrieving the tasks from the database: ',
+        err
+      );
+      res.sendStatus(500); // tell the client something's gone wrong
+    });
 });
 
 module.exports = router;
