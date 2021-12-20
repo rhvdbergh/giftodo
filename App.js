d0c882e70005157cfc12b769d2b978de97dce7cc
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-12-20 15:22:53 -0600

    fetch receives api

--- App.js
+++ App.js
@@ -1,7 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  // on page load:
+  useEffect(() => {
+    fetch('http://localhost:5000/api/task')
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log('Error in fetch: ', err));
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
