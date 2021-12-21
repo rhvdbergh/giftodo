fbff7c3e7f169ec0a1f03a071438ad1e5676a033
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-12-20 21:29:23 -0600

    bottom tab

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
+import { useState } from 'react';
 import { StyleSheet, SafeAreaView } from 'react-native';
+import { ThemeProvider, TabView, Tab } from 'react-native-elements';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import TaskListView from './Components/TaskListView/TaskListView';
 
 export default function App() {
+  // local state for tab index
+  const [tabIndex, setTabIndex] = useState(0);
+
   return (
-    <SafeAreaView style={styles.container}>
-      <TaskListView />
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <TaskListView />
+        <Tab
+          value={tabIndex}
+          onChange={(event) => setTabIndex(event)}
+          indicatorStyle={{
+            backgroundColor: 'white',
+            height: 3,
+          }}
+          variant="primary"
+        >
+          <Tab.Item
+            title="Account"
+            titleStyle={{ fontSize: 12 }}
+            icon={{ name: 'user', type: 'font-awesome-5', color: 'white' }}
+          />
+          <Tab.Item
+            title="Gifs"
+            titleStyle={{ fontSize: 12 }}
+            icon={{
+              name: 'grin-squint',
+              type: 'font-awesome-5',
+              color: 'white',
+            }}
+          />
+          <Tab.Item
+            title="Tasks"
+            titleStyle={{ fontSize: 12 }}
+            icon={{
+              name: 'tasks',
+              type: 'font-awesome-5',
+              color: 'white',
+            }}
+          />
+        </Tab>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
