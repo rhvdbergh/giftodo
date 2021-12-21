ca7a58c77bf7482bfd3436463cc3b082e1a764f5
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-12-21 09:23:50 -0600

    some styling, buttons

--- Components/AddPage/AddPage.jsx
+++ Components/AddPage/AddPage.jsx
@@ -4,8 +4,16 @@ import {
   ScrollView,
   TextInput,
   ActivityIndicator,
+  View,
 } from 'react-native';
-import { colors, Slider, Text, Image, CheckBox } from 'react-native-elements';
+import {
+  colors,
+  Slider,
+  Text,
+  Image,
+  CheckBox,
+  Button,
+} from 'react-native-elements';
 import DateTimePicker from '@react-native-community/datetimepicker';
 
 // set up an empty task object to start with
@@ -26,20 +34,20 @@ function AddPage() {
 
   return (
     <ScrollView style={styles.container}>
-      <Text>Task Name</Text>
+      <Text style={styles.text}>Task Name</Text>
       <TextInput
         placeholder="Enter Task Name"
         autoFocus
         onChangeText={(value) => setTask({ ...task, name: value })}
       />
-      <Text>Task Description</Text>
+      <Text style={styles.text}>Task Description</Text>
       <TextInput
-        style={{ height: 200 }}
+        style={{ height: 100 }}
         placeholder="Enter Task Description"
         multiline
         onChangeText={(value) => setTask({ ...task, description: value })}
       />
-      <Text>Select Priority: {task.priority}</Text>
+      <Text style={styles.text}>Select Priority: {task.priority}</Text>
       <Slider
         value={task.priority}
         onValueChange={(val) => setTask({ ...task, priority: val })}
@@ -52,23 +60,66 @@ function AddPage() {
         minimumTrackTintColor={colors.primary}
         maximumTrackTintColor={colors.grey4}
       />
-      <CheckBox
-        title="Due date"
-        checked={hasDueDate}
-        onPress={() => setHasDueDate(!hasDueDate)}
-      />
-      <DateTimePicker
-        value={task.due_date ?? new Date()}
-        disabled={!hasDueDate}
-        mode={'datetime'}
-        display="default"
-        onChange={(evt, date) => console.log(`event fired with: `, date)}
-      />
-      <Image
-        source={{ uri: task.gif_url }}
-        containerStyle={styles.gif}
-        PlaceholderContent={<ActivityIndicator />}
-      />
+      <View style={styles.dateContainer}>
+        <CheckBox
+          textStyle={styles.text}
+          title="Due date"
+          checked={hasDueDate}
+          onPress={() => setHasDueDate(!hasDueDate)}
+        />
+        <DateTimePicker
+          value={task.due_date ?? new Date()}
+          disabled={!hasDueDate}
+          mode={'datetime'}
+          display="default"
+          onChange={(evt, date) => setTask({ ...task, due_date: date })}
+        />
+      </View>
+      <View style={styles.gifContainer}>
+        <Button
+          title="Generate Gif"
+          buttonStyle={{
+            backgroundColor: colors.primary,
+            borderRadius: 3,
+          }}
+          containerStyle={{
+            width: 200,
+            marginHorizontal: 50,
+            marginVertical: 10,
+          }}
+        />
+        <Image
+          source={{ uri: task.gif_url }}
+          containerStyle={styles.gif}
+          PlaceholderContent={<ActivityIndicator />}
+        />
+      </View>
+      <View style={styles.buttonbox}>
+        <Button
+          title="Cancel"
+          buttonStyle={{
+            backgroundColor: colors.error,
+            borderRadius: 3,
+          }}
+          containerStyle={{
+            width: '30%',
+            marginHorizontal: 10,
+            marginVertical: 10,
+          }}
+        />
+        <Button
+          title="Add"
+          buttonStyle={{
+            backgroundColor: colors.primary,
+            borderRadius: 3,
+          }}
+          containerStyle={{
+            width: '30%',
+            marginHorizontal: 10,
+            marginVertical: 10,
+          }}
+        />
+      </View>
     </ScrollView>
   );
 }
@@ -78,9 +129,26 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
+  dateContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  gifContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
   gif: {
     width: '70%',
     height: 200,
+  },
+  text: {
+    fontSize: 18,
+  },
+  buttonbox: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
 });
 
