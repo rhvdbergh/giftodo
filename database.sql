e2092afddd2f6460b8e3d7d178036e67495f1884
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-12-20 20:21:01 -0600

    adds priority, changes img to gif in db

--- database.sql
+++ database.sql
@@ -5,15 +5,16 @@ CREATE TABLE "task" (
 	"id" SERIAL PRIMARY KEY,
 	"name" VARCHAR(255) NOT NULL,
 	"description" TEXT,
+  "priority" INTEGER DEFAULT 5,
 	"complete" BOOLEAN NOT NULL DEFAULT false,
 	"created" TIMESTAMP DEFAULT NOW(),
-	"due_date" DATE,
-	"img_url" VARCHAR(1000)
+	"due_date" TIMESTAMP,
+	"gif_url" VARCHAR(1000)
 );
 
 INSERT INTO "task" 
-("name", "description", "due_date")
+("name", "description", "due_date", "priority")
 VALUES
-('wash the car', 'use soap and wash the car; make sure to wash the salt off the underside', '2022-01-23'),
-('feed the cats', 'feed the cats with yummy food', '2022-02-12'),
-('RN todo', 'build a React Native ToDo app', '2021-12-31');
+('wash the car', 'use soap and wash the car; make sure to wash the salt off the underside', '2022-01-23', 3),
+('feed the cats', 'feed the cats with yummy food', '2022-02-12', 5),
+('RN todo', 'build a React Native ToDo app', '2021-12-31', 7);
